b5a6a894d292587752e6ead357e67ef7f9a11c01
dhanushd7 <87329959+dhanushd7@users.noreply.github.com>
2023-02-19 00:13:58 +0530

    changed the value of sample variable

--- add.cpp
+++ add.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
     int a ,b , c;
     int sum;
-    int sample = 10;
+    int sample = 11;
     cout<<"enter three numbers : ";
     cin>>a>>b>>c;
     sum = a+b+c;

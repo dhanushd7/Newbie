a6e3642e76a37c8acfa8440861d83f29e93b3105
danxx <dhanush250901@gmail.com>
2023-02-19 00:06:16 +0530

    created new variable sample

--- add.cpp
+++ add.cpp
@@ -4,11 +4,17 @@ using namespace std;
 int main(){
     int a ,b , c;
     int sum;
+    int sample = 10;
     cout<<"enter three numbers : ";
-    cin>>a>>b>>;
+    cin>>a>>b>>c;
     sum = a+b+c;
 
+    sum=sum+sample;
+
+
+
     cout<<sum;
+
 
 
 }
